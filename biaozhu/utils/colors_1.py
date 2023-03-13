ecf06da30a46f9f3f9c88d0d3f3e3417a093254e
chensihuo <105424830+chenyiadam@users.noreply.github.com>
2023-03-13 17:32:02 +0800

    Add files via upload

---
+++ biaozhu/utils/colors_1.py
@@ -0,0 +1,25 @@
+color_mapping_1 = [
+    {'bg': '#3399ff',
+     'fg': 'black'},
+    {'bg': '#00FFFF',
+     'fg': 'black'},
+    {'bg': '#ffff1a',
+     'fg': 'black'},
+    {'bg': '#FF1493',
+     'fg': 'white'},
+    {'bg': '#FF8C00',
+     'fg': 'white'},
+    {'bg': '#cc33ff',
+     'fg': 'white'},
+    {'bg': '#f5f5dc',
+     'fg': 'black'},
+    {'bg': '#6600ff',
+     'fg': 'white'},
+    {'bg': '#ff3399',
+     'fg': 'white'},
+    {'bg': '#4dff4d',
+     'fg': 'black'}
+] # #000000
+
+    
+  
